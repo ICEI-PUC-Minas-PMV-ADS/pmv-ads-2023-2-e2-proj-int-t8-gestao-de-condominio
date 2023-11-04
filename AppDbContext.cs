0ee4d87b1a315702c55b8858ab20136f20434d59
Jonatas Brum <116739381+Jonatas30@users.noreply.github.com>
2023-11-04 12:55:09 -0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arpagen.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+      
+        public DbSet<Morador> Moradores { get; set; }
+        public DbSet<Login> Login { get; set; }
+        //public DbSet<Consumo> Consumos { get; set; }
+       
+    }
+}
