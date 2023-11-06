ca5e08dc46d47c86358a135ed474da367ad2658a
GeorgeNeb <georgelucas1250@protonmail.com>
2023-11-05 22:06:41 -0300

    telainicial

---
+++ src/pages/telainicial/telainicial.js
@@ -0,0 +1,13 @@
+
+
+function Buttonvoltar() {
+    window.location.href = "../homescreen/homescreen.html";
+}
+
+function Buttoncarros() {
+    window.location.href = "../carros/carros.html";
+}
+
+function Areagourmet() {
+    window.location.href = "../areagourmet/areagourmet.html";
+}
